b5b05cab1886811f3d6ef7a095119688e55bb6c1
Marek <marekbaranski0@gmail.com>
2025-03-10 19:50:36 +0100

    Add navbar to base template and update styles; configure settings for template directory

--- student_survey/student_survey/settings.py
+++ student_survey/student_survey/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'student_survey.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
